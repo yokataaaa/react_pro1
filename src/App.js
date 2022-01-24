ecb9f078f0e25f59743585aa5950106e8ca7f7de
LAPTOP-7CH5KCTU\dlawp <dlawpgur84@gmail.com>
2022-01-24 15:03:28 +0900

    2022-01-24

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function random(n) {
 }
 
 function App() {
-//   const [num, setNum] = useState(1);
-//   const [sum, setSum] = useState(0);
+  //   const [num, setNum] = useState(1);
+  //   const [sum, setSum] = useState(0);
   const [gameHistory, setGameHistory] = useState([]);
   // const [numOther, setNumOther] = useState(1);
   // const [sumOther, setSumOther] = useState(0);
@@ -39,15 +39,28 @@ function App() {
     <div className="App">
       <div>
         <img className="App-logo" src={logo} alt="주사위게임 로고" />
-        <h1 className="App-title">주사위게임</h1>     
+        <h1 className="App-title">주사위게임</h1>
         <div>
-            <Button className="App-button" onClick={handleRollClick} color="blue">던지기</Button>
-            <Button className="App-button" onClick={handleClearClick} color="red">처음부터</Button>
+          <Button className="App-button" onClick={handleRollClick} color="blue">
+            던지기
+          </Button>
+          <Button className="App-button" onClick={handleClearClick} color="red">
+            처음부터
+          </Button>
         </div>
       </div>
       <div className="App-boards">
-        <Board className="" name="나" color="blue" /*num={num} sum={sum}*/ gameHistory={gameHistory}/>
-        <Board name="상대" color="red" /*num={numOther} sum={sumOther}*/ gameHistory={gameHistoryOther}/>
+        <Board
+          className=""
+          name="나"
+          color="blue"
+          /*num={num} sum={sum}*/ gameHistory={gameHistory}
+        />
+        <Board
+          name="상대"
+          color="red"
+          /*num={numOther} sum={sumOther}*/ gameHistory={gameHistoryOther}
+        />
       </div>
     </div>
   );
